bef54766ceb3c14806f276a71bd1c3ebdfe824a5
Rminollari <rminollari@gmail.com>
2021-12-17 18:41:35 +0100

    modified searchbar and added Slider

--- src/components/FilteredSearch.jsx
+++ src/components/FilteredSearch.jsx
@@ -1,25 +1,49 @@
 import { BiSearchAlt } from 'react-icons/bi'
+import { FormSelect } from "react-bootstrap"
 
 const FilteredSearch = () => {
 
     // CSS styles
 
     const containerStyle = {
-        display: "inline-block",
-        fontSize: "2rem",
+        display: "flex",
+        alignItems: 'center',
+        fontSize: "1.3rem",
         margin: "0 auto",
         paddingLeft: "15px",
         paddingRight: "15px",   
-        width: "auto",
+        width: "1000px",
         height: "65px",
-        backgroundColor: "grey",
+        backgroundColor: "#C2C2C2",
         borderRadius: "5px"
     }
 
     return (
         <div style={containerStyle}>
-            <BiSearchAlt />
-            <input/>
+            <BiSearchAlt  style={ { fontSize: "2rem" } } />
+            Minimum Year: <FormSelect size='sm' style={ { display: "inline-block", width: "100px", margin: "10px" } } >
+                <option> 2005 </option>
+                <option> 2006 </option>
+                <option> 2007 </option>
+                <option> 2008 </option>
+                <option> 2009 </option>
+                <option> 2010 </option> 
+            </FormSelect>
+            Maximum Year: <FormSelect size='sm' style={ { display: "inline-block", width: "100px", margin: "10px" } } >
+                <option> 2005 </option>
+                <option> 2006 </option>
+                <option> 2007 </option>
+                <option> 2008 </option>
+                <option> 2009 </option>
+                <option> 2010 </option> 
+            </FormSelect>
+            Brand: <FormSelect size='sm' style={ { display: "inline-block", width: "100px", margin: "10px" } } >
+                <option> Mercedes-Benz </option>
+                <option> BMW </option>
+                <option> Toyota </option>
+                <option> Ford </option>
+                <option> Volkswagen </option>
+            </FormSelect>
         </div>
     )
 }
--- src/routes/SearchCar.jsx
+++ src/routes/SearchCar.jsx
@@ -1,11 +1,15 @@
 import Introduction from "../components/Introduction"
 import FilteredSearch from "../components/FilteredSearch"
+import Slider from "../components/Slider"
+import Footer from "../components/Footer"
 
 const SearchCar = () => {
     return (
         <div>
             <Introduction />
             <FilteredSearch />
+            <Slider sliderTitle="Search for your car" />
+            <Footer />
         </div>
     )
 }
